e77848a4395d162f9589bcb9f231dc72b2784f34
Ibad Qureshi <Ibad Qureshi>
2017-01-26 23:53:43 -0500

    working on company

--- app/src/main/java/com/example/dellpc/recruimentsystem/CompanyActivity.java
+++ app/src/main/java/com/example/dellpc/recruimentsystem/CompanyActivity.java
@@ -1,16 +1,23 @@
 package com.example.dellpc.recruimentsystem;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class CompanyActivity extends AppCompatActivity {
 
-
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
+
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.mFragStudent, new CompanyPostFragment()).commit();
     }
 }
